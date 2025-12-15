d0b1b0d41acc780665b6632d4f261171a51bcf12
Ilja Leyberman <7ilya@gmx.de>
2025-12-15 08:20:50 +0100

    Enhance overlay with navigation and dynamic content
    Added navigation to the overlay component, allowing users to switch between 'about', 'motivation', and 'techstack' sections. Updated overlay styles for improved navigation highlighting and layout. Introduced dynamic content loading for the 'about' section based on the current language, including a new German translation file and image support. Refactored translation logic to support dynamic script loading and language state management.

--- src/main/resources/static/components/overlay/overlay.js
+++ src/main/resources/static/components/overlay/overlay.js
@@ -7,13 +7,32 @@ const OVERLAY = {
         await OVERLAY.close();
     });
 
+    $('#overlay-navi li').off().on('click', OVERLAY.navigate);
+
 
     return this;
   },
   show: async () => {
+    const content = await TRANSLATION.loadAbout();
+    await OVERLAY.updateContent(content);
+
     $('#overlay').removeClass('hidden');
   },
   close: async () => {
     $('#overlay').addClass('hidden');
+  },
+  navigate: async (e) => {
+     const $item = $(e.currentTarget);
+     const i = $item.data('item');
+
+     $item.parent().find('li').removeClass('active');
+     $item.addClass('active');
+
+    const el = $(`#overlay-content h2[data-item="${i}"]`);
+    $('#overlay-content').animate({ scrollTop: el.offset().top }, 600);
+  },
+  updateContent: async (content) => {
+    console.log('about content: ', content)
+   // console.log('about_cnt', about_cnt)
   }
 }
---
+++ src/main/resources/static/translations/about.de.js
@@ -0,0 +1,20 @@
+export const ABOUT_CNT = {
+  about: {
+    left: `
+            Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.
+            Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.
+
+            Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.
+            Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.    
+    `,
+    right: "/img/image.jpeg"
+  },
+  motivation: {
+    left: `Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.`,
+    right: `Die Polizei Dortmund ermittelt wegen Raubes und sucht Zeugen, die zur Tatzeit verdächtige Feststellungen gemacht haben oder Hinweise zu dem Täter geben können. Hinweise nimmt die Kriminalwache unter der Telefonnummer 0231/132-7441 entgegen.`
+  },
+  techstack: {
+    left: `Nach kurzer Zeit hielt das Taxi auf Höhe der Heyden-Rynsch-Straße an. Der Fahrer stieg aus und zog den 51-Jährigen an seiner Umhängetasche aus dem Fahrzeug. Während der Geschädigte auf dem Boden lag, schlug der Unbekannte mehrfach mit Fäusten auf ihn ein und entriss ihm die Umhängetasche samt Bargeld und Mobiltelefon. Anschließend stieg er wieder in das Taxi und fuhr davon. Dabei wurde der 51-jährige Marsberger leicht verletzt.`,
+    right: `Die Polizei Dortmund ermittelt wegen Raubes und sucht Zeugen, die zur Tatzeit verdächtige Feststellungen gemacht haben oder Hinweise zu dem Täter geben können. Hinweise nimmt die Kriminalwache unter der Telefonnummer 0231/132-7441 entgegen.`
+  }
+}
